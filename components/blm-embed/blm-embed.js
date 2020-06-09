9d312e3fd73c4439fe13e2a7196e54504173da5c
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-09 22:55:29 +1200

    Support embed in modal

--- components/blm-embed/blm-embed.js
+++ components/blm-embed/blm-embed.js
@@ -3,19 +3,20 @@
 	const { get } = RaiselyComponents.Common;
 
 	return function BlmEmbed(props) {
+		const { name, url, getValues } = props;
 		const [content, setContent] = React.useState();
 		const [hasCopied, setHasCopied] = React.useState();
 
-		const values = props.getValues();
-		const query = getQuery(get(props, 'router.location.search'));
+		const values = (getValues && typeof getValues === 'function') && getValues();
+		const query = get(props, 'router.location.search') && getQuery(get(props, 'router.location.search'));
 		const style = `background: black; color: white; padding: 1rem; width: 100%;`;
 
-		const charity = props.name || query.name || values.name || 'support #BLM';
-		const url = props.url || query.url || values.url || 'https://raisely-blm.raisely.com/';
+		const charityName = name || query.name || values.name || 'support #BLM';
+		const charityUrl = url || query.url || values.url || 'https://raisely-blm.raisely.com/';
 
 		const defaultHtml = `<div style="${style}">
 	<p style="font-size: 1rem; text-align: left;">✊🏿✊🏾✊🏽 We know that our mission can only be achieved when people are safe from violence against their community.
-	<br/>You can support equal rights for all by donating to <a style="color: white; text-decoration: underline;" href="${url}">${charity}</a></p>
+	<br/>You can support equal rights for all by donating to <a style="color: white; text-decoration: underline;" href="${charityUrl}">${charityName}</a></p>
 </div>`;
 
 		const exampleHtml = content || defaultHtml;
