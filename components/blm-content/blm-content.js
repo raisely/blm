1c71e54f1c75d4cfba04afa4221905b2cb3286f3
Luke <luke@agency.sc>
2020-06-15 13:09:46 +1000

    fix button icon zindex bug + share shares to inline icons

--- components/blm-content/blm-content.js
+++ components/blm-content/blm-content.js
@@ -88,7 +88,7 @@ RaiselyComponents => {
 						<h2>📣&nbsp;Spread the word</h2>
 						<RaiselyShare
 							theme="filled"
-							size="normal"
+							size="icon"
 							networks={['facebook', 'twitter', 'email', 'whatsapp', 'linkedin', 'link']}
 							global={global}
 						/>
@@ -108,6 +108,7 @@ RaiselyComponents => {
 					href={org.donateUrl}
 					rel="noopener"
 					target="_blank"
+					id="highlight-org"
 				>
 					<div className="highlight-org__edge">
 						<span />
@@ -134,7 +135,7 @@ RaiselyComponents => {
 					</div>
 				</a>
 				<div className="highlight-actions">
-					<button
+					<a
 						className="link-button"
 						onClick={() => data &&
 							setHighlight(
@@ -146,10 +147,11 @@ RaiselyComponents => {
 								)
 							)
 						}
+						href="#highlight-org"
 					>
 						<i className="material-icons loop">loop</i>
 						Show me another
-					</button>
+					</a>
 					<span className="highlight-actions__divider">-</span>
 					{/* <button
 						className="link-button"
